d17c3e6d1094321def273315939a955f3dcf2cfa
Keith Marshall <keithmarshall@users.sourceforge.net>
2010-01-23 17:01:53 +0000

    Avoid segmentation faults when processing invalid data streams.

--- src/pkginet.cpp
+++ src/pkginet.cpp
@@ -374,7 +374,17 @@ public pkgInternetStreamingAgent, public pkgLzmaArchiveStream
 pkgInternetLzmaStreamingAgent::pkgInternetLzmaStreamingAgent
 ( const char *local_name, const char *dest_specification ):
 pkgInternetStreamingAgent( local_name, dest_specification ),
-pkgLzmaArchiveStream( -1 ){}
+/*
+ * Note that, when we come to initialise the lzma streaming component
+ * of this derived class, we will be streaming directly from the internet,
+ * rather than from a file stream, so we don't require a file descriptor
+ * for the input stream; however, the class semantics still expect one.
+ * To avoid accidental association with an existing file stream, we
+ * use a negative value, (which is never a valid file descriptor);
+ * however, we must not choose -1, since the class implementation
+ * will decline to process the stream; hence, we choose -2.
+ */
+pkgLzmaArchiveStream( -2 ){}
 
 int pkgInternetLzmaStreamingAgent::GetRawData( int fd, uint8_t *buf, size_t max )
 {
